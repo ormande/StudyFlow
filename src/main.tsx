efb848c41b2958d7ad8a67070c6f5582bc2639aa
ormande <playerone01kp@gmail.com>
2025-12-10 16:38:52 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ToastProvider } from './contexts/ToastContext'; // <--- Importe aqui
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ToastProvider> {/* <--- Envolva o App aqui */}
+      <App />
+    </ToastProvider>
   </StrictMode>
 );
